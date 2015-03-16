2788a69457b56d22fcf21ee22a08c05d7336d1ed
Duyet Nguyen <duyetlnguyen@gmail.com>
2015-03-15 18:17:17 -0700

    Initial setup

---
+++ backend/boot.rb
@@ -0,0 +1,37 @@
+env = ENV['RACK_ENV'] || 'development'
+
+# load dependencies
+Bundler.require :default, env.to_sym
+
+# initialize application
+Application = Hashie::Mash.new
+
+# env
+Application.env = env
+
+# root
+Application.root = File.expand_path File.dirname(__FILE__)
+
+# connect db
+ActiveRecord::Base.establish_connection(
+  YAML.load_file(File.join(Application.root, 'config', 'database.yml'))[Application.env]
+)
+
+# load paths
+$LOAD_PATH.unshift           Application.root
+$LOAD_PATH.unshift File.join(Application.root, 'lib')
+
+# config
+I18n.enforce_available_locales = false
+I18n.default_locale = :en
+
+# load files
+[
+  'lib/*.rb',
+  'lib/models/**/*.rb',
+  'lib/services/**/*.rb'
+].each do |path|
+  Dir["#{Application.root}/#{path}"].each do |file|
+    require file
+  end
+end
---
+++ backend/spec/spec_setup.rb
@@ -0,0 +1,34 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/db/'
+  add_filter '/spec/'
+  add_filter '/config/'
+
+  add_group 'models', 'lib/models'
+  add_group 'services', 'lib/services'
+end
+
+require './boot'
+Dir['spec/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include Mail::Matchers
+  config.color_enabled = true
+  config.tty = true
+  config.order = 'random'
+
+  config.before :suite do
+    FactoryGirl.find_definitions
+
+    begin
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with :truncation
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean_with :truncation
+    end
+  end
+end
