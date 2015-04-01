4dc1e61da1666939a710be9c8ab25de9dcf2561b
Duyet Nguyen <duyetlnguyen@gmail.com>
2015-03-31 22:01:54 -0700

    Add additional spec helper methods

--- backend/spec/support/spec_helpers.rb
+++ backend/spec/support/spec_helpers.rb
@@ -1,6 +1,18 @@
 module SpecHelpers
+  module Common
+    extend ActiveSupport::Concern
+
+    included do
+      let(:rand_str) { Faker::Lorem.words.join('') }
+      let(:rand_num) { rand(1..50) }
+    end
+  end
+end
+
+module SpecHelpers
   module Models
     extend ActiveSupport::Concern
+    include SpecHelpers::Common
 
     included do
       let(:model_args) { [described_class.to_s.underscore.to_sym] }
