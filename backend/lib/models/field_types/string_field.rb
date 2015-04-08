11fc0283de7fa98394211a2e8fc2aec80cfe017b
Duyet Nguyen <duyetlnguyen@gmail.com>
2015-04-07 22:24:45 -0700

    Add condition methods for field types

--- backend/lib/models/field_types/string_field.rb
+++ backend/lib/models/field_types/string_field.rb
@@ -4,4 +4,21 @@ class StringField < Field
   def self.value_column
     :string_value
   end
+
+  # overridden
+  def self.lt?(issue, value)
+    not_supported!(LessThan)
+  end
+
+  def self.gt?(issue, value)
+    not_supported!(GreaterThan)
+  end
+
+  def self.lte?(issue, value)
+    not_supported!(LessThanEqual)
+  end
+
+  def self.gte?(issue, value)
+    not_supported!(GreaterThanEqual)
+  end
 end
