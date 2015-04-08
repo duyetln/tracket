19b6c22ff69f956ba819cf2ecc95e1ece8634e06
Duyet Nguyen <duyetlnguyen@gmail.com>
2015-04-07 22:49:27 -0700

    Add condition checking methods for clauses

--- backend/lib/models/clause_types/and_clause.rb
+++ backend/lib/models/clause_types/and_clause.rb
@@ -1,4 +1,8 @@
 require 'models/clause'
 
 class AndClause < Clause
+  def satisfy?(issue)
+    conditions.all? { |c| c.satisfy?(issue) } &&
+    clauses.all? { |c| c.satisfy?(issue) }
+  end
 end
--- backend/lib/models/clause_types/or_clause.rb
+++ backend/lib/models/clause_types/or_clause.rb
@@ -1,4 +1,8 @@
 require 'models/clause'
 
 class OrClause < Clause
+  def satisfy?(issue)
+    conditions.any? { |c| c.satisfy?(issue) } ||
+    clauses.any? { |c| c.satisfy?(issue) }
+  end
 end
