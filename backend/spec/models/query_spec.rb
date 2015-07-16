fe7c972b72711806bc964755e1134de91c21e383
Duyet Nguyen <duyetlnguyen@gmail.com>
2015-07-16 02:08:36 +0000

    Add addtional query specs

--- backend/spec/models/query_spec.rb
+++ backend/spec/models/query_spec.rb
@@ -21,7 +21,10 @@ describe Query do
     end
   end
 
-  describe '#issues' do
+  describe '#issues', :no_db_clean do
+    let(:issues) { model.issues(:all) }
+    let(:model) { described_class.new(project: project, constraint: constraint) }
+
     attr_reader :project, :count
     attr_reader :string_field, :text_field, :integer_field, :decimal_field, :date_time_field, :option_field
     attr_reader :string_value, :text_value, :integer_value, :decimal_value, :date_time_value, :option_value
@@ -52,9 +55,7 @@ describe Query do
       @option_value = @option_field.options.first.id
 
       {
-        @string_field => @string_value,
         @text_field => @text_value,
-        @integer_field => @integer_value,
         @decimal_field => @decimal_value,
         @date_time_field => @date_time_value,
         @option_field => @option_value
@@ -62,66 +63,120 @@ describe Query do
         @count.times do
           issue = FactoryGirl.build :issue, project: @project
           issue[f] = v
+          issue[@string_field] = @string_value
+          issue[@integer_field] = @integer_value
           issue.save!
         end
       end
     end
 
     shared_examples 'query' do
-      let(:model) { described_class.new(project: project, constraint: Equal.new(field: field, value: value)) }
-      let(:issues) { model.issues(:all) }
-
       it 'retrieves issues correctly' do
-        expect(issues.size).to eq(count)
+        expect(issues.size).to eq(expected_count)
         issues.each do |issue|
           expect(issue.satisfy?(model.constraint)).to eq(true)
         end
       end
 
       it 'counts issues correctly' do
-        expect(model.issues(:count)).to eq(count)
+        expect(model.issues(:count)).to eq(expected_count)
       end
     end
 
-    context 'single condition', :no_db_clean do
+    context 'condition constraint' do
+      let(:expected_count) { count }
+
       context 'string field' do
-        let(:field) { string_field }
-        let(:value) { string_value }
+        let(:expected_count) { count * 4 }
+        let(:constraint) { Equal.new field: string_field, value: string_value }
 
         include_examples 'query'
       end
 
       context 'text field' do
-        let(:field) { text_field }
-        let(:value) { text_value }
+        let(:constraint) { Equal.new field: text_field, value: text_value }
 
         include_examples 'query'
       end
 
       context 'integer field' do
-        let(:field) { integer_field }
-        let(:value) { integer_value }
+        let(:expected_count) { count * 4 }
+        let(:constraint) { Equal.new field: integer_field, value: integer_value }
 
         include_examples 'query'
       end
 
       context 'decimal field' do
-        let(:field) { decimal_field }
-        let(:value) { decimal_value }
+        let(:constraint) { Equal.new field: decimal_field, value: decimal_value }
 
         include_examples 'query'
       end
 
       context 'date time field' do
-        let(:field) { date_time_field }
-        let(:value) { date_time_value }
+        let(:constraint) { Equal.new field: date_time_field, value: date_time_value }
 
         include_examples 'query'
       end
 
       context 'option field' do
-        let(:field) { option_field }
-        let(:value) { option_value }
+        let(:constraint) { Equal.new field: option_field, value: option_value }
+
+        include_examples 'query'
+      end
+    end
+
+    context 'clause constraint' do
+      context 'and clause' do
+        let(:expected_count) { count }
+        let :constraint do
+          constraint = AndClause.new
+          constraint.conditions << Equal.new(field: string_field, value: string_value)
+          constraint.conditions << Equal.new(field: decimal_field, value: decimal_value)
+          constraint
+        end
+
+        include_examples 'query'
+      end
+
+      context 'or clause' do
+        let(:expected_count) { count * 2 }
+        let :constraint do
+          constraint = OrClause.new
+          constraint.conditions << Equal.new(field: date_time_field, value: date_time_value)
+          constraint.conditions << Equal.new(field: option_field, value: option_value)
+          constraint
+        end
+
+        include_examples 'query'
+      end
+
+      context 'disjunctive clause' do
+        let(:expected_count) { count * 2 }
+        let :constraint do
+          and_clause = AndClause.new
+          and_clause.conditions << Equal.new(field: string_field, value: string_value)
+          and_clause.conditions << Equal.new(field: option_field, value: option_value)
+
+          constraint = OrClause.new
+          constraint.conditions << Equal.new(field: decimal_field, value: decimal_value)
+          constraint.clauses << and_clause
+          constraint
+        end
+
+        include_examples 'query'
+      end
+
+      context 'conjunctive clause' do
+        let(:expected_count) { count }
+        let :constraint do
+          or_clause = OrClause.new
+          or_clause.conditions << Equal.new(field: string_field, value: string_value)
+          or_clause.conditions << Equal.new(field: option_field, value: option_value)
+
+          constraint = AndClause.new
+          constraint.conditions << Equal.new(field: date_time_field, value: date_time_value)
+          constraint
+        end
 
         include_examples 'query'
       end
