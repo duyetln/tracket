40222889e377d4086a9649a19d8f04be73ee893b
Duyet Nguyen <duyetlnguyen@gmail.com>
2016-08-21 13:41:59 -0700

    Add Pageable module

---
+++ backend/lib/models/shared/pageable.rb
@@ -0,0 +1,22 @@
+module Pageable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def page(page = nil, opts = {})
+      page = page.to_i
+      size = opts[:size].to_i
+      padn = opts[:padn].to_i
+
+      if page > 0
+        size =  size > 0 && size || 25
+        padn =  padn > 0 && padn || 0
+        offs = (page - 1) * size
+        offset(offs).limit(size + padn)
+      else
+        all
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.include Pageable
